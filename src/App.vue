1ef8b1c580bcc76366c7856a83f1627acb8ad72d
duzhe <363958265@qq.com>
2025-03-12 10:58:51 +0800

    初始项目提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+/**
+ * 应用根组件
+ */
+import AppLayout from './components/layout/AppLayout.vue'
+</script>
+
+<template>
+  <AppLayout />
+</template>
+
+<style>
+/* 全局样式在assets/styles/main.scss中定义 */
+</style>
